7f308fc4f38dc4ef0e6ce234364c304a86f24846
MatiasVinales <mati_654@outlook.es>
2020-11-30 22:45:43 -0300

    Se agregan mas cambios a la app

--- sistemaRepuestos/Models/BBDD.cs
+++ sistemaRepuestos/Models/BBDD.cs
@@ -5,19 +5,36 @@ using System.Web;
 
 namespace sistemaRepuestos.Models
 {
-    public class BBDD{
+    public class BBDD
+    {
         private static List<Producto> tablaProductos = new List<Producto> {
             new Producto("descripcion", 2),
             new Producto("otra descripcion",14)
         };
 
 
-        public static List<Producto> ListarProductos() {
+        public static List<Producto> ListarProductos()
+        {
             var listado = from p in tablaProductos
                           orderby p.descripcion, p.stock
                           select p;
 
             return listado.ToList();
+
+        }
+
+        public static String ingresarProducto(Producto p)
+        {
+
+            try
+            {
+                tablaProductos.Add(p); 
+                return "Producto insertado";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
 
         }
 
